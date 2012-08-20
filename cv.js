d072fc9e7f69a5d23a808f6b7781a2bb7db9c0d6
dpacaud <serty2@gmail.com>
2012-08-20 23:07:03 +0200

    adding comments and indenting

--- cv.js
+++ cv.js
@@ -1,14 +1,18 @@
+
+/************************************************************************/
+/*				HumanBeing object definition							*/
+/************************************************************************/
 function humanBeing(_firstName,_lastName,_birthdate,_gender) {
-	this.firstName = _firstName;
-	this.lastName = _lastName;
-	this.birthDate = _birthdate;
-	this.gender = _gender
-	this.achievements = new Array();
+	this.firstName 		= _firstName;
+	this.lastName 		= _lastName;
+	this.birthDate 		= _birthdate;
+	this.gender 		= _gender
+	this.achievements 	= new Array();
 	console.log("new " + _gender + " human created : \n" + _firstName + " " + _lastName + " on " + _birthdate.toDateString())
 };
 
 humanBeing.prototype.setLocation = function(_location) {
-	this.location = _location;
+	this.location 		= _location;
 	console.log("location set to : " + _location);
 };
 
@@ -17,33 +21,44 @@ humanBeing.prototype.addAchievement = function(_achievement) {
 	_achievement.print();
 };
 
-
+/************************************************************************/
+/*				Achievement object definition							*/
+/************************************************************************/
 function achievement (_name,_description,_year) {
-	this.name = _name;
-	this.description = _description;
-	this.year = _year;
+	this.name 			= _name;
+	this.description 	= _description;
+	this.year 			= _year;
 };
 
 achievement.prototype.print = function() {
 	console.log(this.name +  ( this.description ? "\n" + this.description : ""));
 };
 
-function job(_company,_companyWebsite,_companyDescription,_title,_responsibilities,_startDate,_endDate) {
-	this.company = _company;
-	this.companyWebsite = _companyWebsite;
-	this.companyDescription = _companyDescription;
-	this.title = _title;
-	this.responsibilities = _responsibilities;
-	this.startDate = _startDate;
-	this.endDate = _endDate;
-	this.missions = new Array();
+/************************************************************************/
+/*				Company object definition								*/
+/************************************************************************/
+function company (_name,_website,_description) {
+	this.name 			= _name;
+	this.website 		= _website
 };
 
 
+/************************************************************************/
+/*				Job object definition									*/
+/************************************************************************/
 
+function job(_company, _title, _responsibilities, _startDate, _endDate) {
+	this.company 			= _company;
+	this.title 				= _title;
+	this.responsibilities 	= _responsibilities;
+	this.startDate 			= _startDate;
+	this.endDate 			= _endDate;
+	this.missions 			= new Array();
+};
 
-function()
-
+/************************************************************************/
+/*				TimeLine object definition								*/
+/************************************************************************/
 function timeLine() {
 
 };
@@ -53,6 +68,10 @@ timeLine.prototype.setYear = function(_year) {
 	console.log("Year set to : " + _year);
 };
 
+
+/************************************************************************/
+/*							Script execution							*/
+/************************************************************************/
 var tl = new timeLine(1982);
 var me = new humanBeing("Damien","Pacaud",new Date(1982,02,24),"Male");
 me.setLocation("Paris, France");
